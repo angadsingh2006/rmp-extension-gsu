bda493e5b06ddae7d17f9626b3a531e1b07c3cc2
Angad Singh <palasingh2006@gmail.com>
2026-06-17 00:25:32 -0400

    updated

--- src/content.js
+++ src/content.js
@@ -5,9 +5,14 @@ const callback = (mutationList, observer) => {
     for ( const mutation of mutationList) {
         if (mutation.type === 'childList') {
             for ( const node of mutation.addedNodes){
-                const text = node.querySelector('[data-property = "instructor"]')?.textContent;
-                split = text.split(',')
-                const message = chrome.runtime.sendMessage({instructorName : split[0]});
+                const information = node.querySelector('[data-property = "instructor"]');
+                if ( information != null){
+                    const text = information.textContent;
+                    const split = text.split(',')
+                    const dataId = information.getAttribute("data-id")
+                    const message = chrome.runtime.sendMessage({instructorName : split[0], id : dataId});
+                } 
+                
             }
 
         }
